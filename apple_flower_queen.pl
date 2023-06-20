310ad81c076012d2512a93b1cde7149507ea926e
desisapoetro <136754615+desisapoetro@users.noreply.github.com>
2023-06-20 16:03:10 +0700

    Improve error recovery

---
+++ apple_flower_queen.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+ 
+# code to print out the words 'Building Dreams'
+print "Building Dreams\n";
+ 
+# define a variable to keep track of how many dreams have been built
+my $dreams_built = 0;
+ 
+# loop 1000 times and increment the number of dreams built
+while ($dreams_built < 1000) {
+ 
+  # code that builds a dream
+  # ...
+  # ...
+ 
+  # increment number of dreams built after building one
+  $dreams_built++;
+}
+ 
+# print out a success message
+print "Yay, $dreams_built dreams built!\n";
